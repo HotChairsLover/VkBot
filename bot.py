fd6c4840a3f0f09a6a8e3a6ca1b8f97d6c002302
PC <kornat041@mail.ru>
2021-07-27 19:17:24 +0500

    добавлены сценарии и обновленны тесты

--- bot.py
+++ bot.py
@@ -5,6 +5,9 @@ from random import randint
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.vk_api import VkApi
 
+import handlers
+import settings
+
 try:
     from settings import GROUP_ID, TOKEN
 except ImportError:
@@ -28,6 +31,15 @@ def loggers_configure():
     bot_logger.setLevel("DEBUG")
 
 
+class UserState:
+    """Состояние пользователя внутри сценария."""
+
+    def __init__(self, scenario_name, step_name, context=None):
+        self.scenario_name = scenario_name
+        self.step_name = step_name
+        self.context = context or {}
+
+
 class Bot:
     """
     Echo bot для вк
@@ -43,6 +55,7 @@ class Bot:
         self.vk_session = VkApi(token=self.token)
         self.longpoller = VkBotLongPoll(self.vk_session, self.group_id)
         self.vk_api = self.vk_session.get_api()
+        self.user_states = dict()  # user_id -> UserState
 
     def run(self):
         """
@@ -62,13 +75,56 @@ class Bot:
         :param event: VkBotEventType
         :return: None
         """
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            random_id = randint(0, 2 ** 10)
-            message = event.object.message['text']
-            peer_id = event.object.message['peer_id']
-            self.vk_api.messages.send(random_id=random_id, peer_id=peer_id,
-                                      message=message)
-            bot_logger.info("Отправленно сообщение")
+        if event.type != VkBotEventType.MESSAGE_NEW:
+            bot_logger.info(f'мы пока не умеем обрабатывать событие такого типа {event.type}')
+            return
+
+        user_id = event.object.message['peer_id']
+        text = event.object.message['text']
+
+        if user_id in self.user_states:
+            text_to_send = self.continue_scenatio(user_id=user_id, text=text)
+        else:
+            for intent in settings.INTENTS:
+                if any(token in text.lower() for token in intent['tokens']):
+                    if intent['answer']:
+                        text_to_send = intent['answer']
+                    else:
+                        text_to_send = self.start_scenario(user_id, intent['scenario'])
+                    break
+            else:
+                text_to_send = settings.DEFAULT_ANSWER
+
+        self.vk_api.messages.send(
+            random_id=randint(0, 2 ** 10),
+            peer_id=user_id,
+            message=text_to_send,
+        )
+
+    def start_scenario(self, user_id, scenario_name):
+        scenario = settings.SCENARIOS[scenario_name]
+        first_step = scenario['first_step']
+        step = scenario['steps'][first_step]
+        text_to_send = step['text']
+        self.user_states[user_id] = UserState(scenario_name=scenario_name, step_name=first_step)
+        return text_to_send
+
+    def continue_scenatio(self, user_id, text):
+        state = self.user_states[user_id]
+        steps = settings.SCENARIOS[state.scenario_name]['steps']
+        step = steps[state.step_name]
+        handler = getattr(handlers, step['handler'])
+        if handler(text=text, context=state.context):
+            next_step = steps[step['next_step']]
+            text_to_send = next_step['text'].format(**state.context)
+            if next_step['next_step']:
+                state.step_name = step['next_step']
+            else:
+                self.user_states.pop(user_id)
+        else:
+            text_to_send = step['failure_text'].format(**state.context)
+
+        return text_to_send
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -1,12 +1,33 @@
 import unittest
-from unittest.mock import patch, Mock, ANY
+from copy import deepcopy
+from unittest.mock import patch, Mock
 
 from vk_api.bot_longpoll import VkBotMessageEvent
 
+import settings
 from bot import Bot
 
 
 class MyTestCase(unittest.TestCase):
+    INPUTS = [
+        'Привет',
+        'А когда?',
+        "Где будет конференция?",
+        "Зарегистрируй меня",
+        "Вениамин",
+        "Мой адрес email@email",
+        "email@email.ru"
+    ]
+
+    EXPECTED_OUTPUTS = [
+        settings.DEFAULT_ANSWER,
+        settings.INTENTS[0]['answer'],
+        settings.INTENTS[1]['answer'],
+        settings.SCENARIOS['registration']['steps']['step1']['text'],
+        settings.SCENARIOS['registration']['steps']['step2']['text'],
+        settings.SCENARIOS['registration']['steps']['step2']['failure_text'],
+        settings.SCENARIOS['registration']['steps']['step3']['text'].format(name='Вениамин', email='email@email.ru')
+    ]
     RAW_EVENT = {
         'type': 'message_new',
         'object': {
@@ -38,22 +59,32 @@ class MyTestCase(unittest.TestCase):
                 bot.on_event.assert_called_with(obj)
                 assert bot.on_event.call_count == count
 
-    def test_on_event(self):
-        event = VkBotMessageEvent(raw=self.RAW_EVENT)
+    def test_run_ok(self):
         send_mock = Mock()
-        with patch('bot.VkApi'):
-            with patch('bot.VkBotLongPoll'):
-                bot = Bot('', '')
-                bot.vk_api = Mock()
-                bot.vk_api.messages.send = send_mock
+        api_mock = Mock()
+        api_mock.messages.send = send_mock
 
-                bot.on_event(event)
+        events = []
+        for input_text in self.INPUTS:
+            event = deepcopy(self.RAW_EVENT)
+            event['object']['message']['text'] = input_text
+            events.append(VkBotMessageEvent(event))
 
-        send_mock.assert_called_once_with(
-            message=self.RAW_EVENT['object']['message']['text'],
-            random_id=ANY,
-            peer_id=self.RAW_EVENT['object']['message']['peer_id']
-        )
+        long_poller_mock = Mock()
+        long_poller_mock.listen = Mock(return_value=events)
+
+        with patch('bot.VkBotLongPoll', return_value=long_poller_mock):
+            bot = Bot("", "")
+            bot.vk_api = api_mock
+            bot.run()
+
+        assert send_mock.call_count == len(self.INPUTS)
+
+        real_outputs = []
+        for call in send_mock.call_args_list:
+            args, kwargs = call
+            real_outputs.append(kwargs['message'])
+        assert real_outputs == self.EXPECTED_OUTPUTS
 
 
 if __name__ == '__main__':
